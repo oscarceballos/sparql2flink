eafbbc93caa95c863d9b75d2c77221dc1c225db0
Oscar Ceballos <oscar.ceballos@correounivalle.edu.co>
2021-07-22 06:43:22 -0500

    Se modifica la cración de la clase Query

--- src/main/java/sparql2flink/mapper/CreateFlinkProgram.java
+++ src/main/java/sparql2flink/mapper/CreateFlinkProgram.java
@@ -24,8 +24,8 @@ public class CreateFlinkProgram {
 
     public void createFlinkProgram() {
         byte data[] = this.flinkProgram.getBytes();
-        Path path = Paths.get("./src/main/java/sparql2flink/out/" + this.fileName + ".java");
-        //Path p = Paths.get("./" + this.fileName + ".java");
+        Path path = Paths.get("../src/main/java/sparql2flink/out/" + this.fileName + ".java");
+        //Path path = Paths.get("../queries/example/" + this.fileName + ".java");
         //Path p = Paths.get("./" + fileName + ".java");
         try (OutputStream out = new BufferedOutputStream(Files.newOutputStream(path, CREATE, TRUNCATE_EXISTING))) {
             out.write(data, 0, data.length);
